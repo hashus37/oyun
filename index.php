2df4177945a69a775c4a643be93cba61cdf9ac31
hashuskahraman <hashus5021@gmail.com>
2023-12-31 14:37:19 +0300

    partials kısmı eklendi çoğu düzeltmeleri yaptım ve recaptcha ekledim daha sonra yeni alanlarda ekledim

--- index.php
+++ index.php
@@ -1,56 +1,22 @@
-   <?php 
-   session_start();
-   ?>
-   
-   <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <link rel="stylesheet" href="style.css">
-        <script type="module">
-            import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
-        
-            // script.js dosyasını ekleyin
-            import './script.js';
-        
-            // Bu kısmı ekleyerek Three.js'i modül olarak ekleyebilirsiniz.
-        </script>
-        
+           <?php
+            include('partials/_header.php');
+           ?>
 
-    </head>
-    <body>
-
-        <div id="threejs-container">    </div>
+           <?php 
+           include('partials/_three-js.php');
+           ?>
         
             <!-- navbar section start -->
-            <section id="navbar">
-                <nav class="navbar navbar-expand-lg shadow-sm">
-                    <div class="container py-2">
-                    <a class="navbar-brand text-danger fw-bold fs-2 anime-bottom" href="#">
-                        <img src="img/logo-removebg-preview-copy.png" alt="" style="height: 50px;">
-                    </a>
-                    <button class="navbar-toggler text-danger bg-danger" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav ms-auto fs-5  ">
-                        <a class="nav-link fw-bold ms-3" aria-current="page" href="#home">Ana Sayfa</a>
-                        <a class="nav-link fw-bold ms-3" href="#services">Hizmetlerimiz</a>
-                        <a class="nav-link fw-bold ms-3" href="#about">Hakkımızda</a>
-                        <a class="nav-link fw-bold ms-3" href="blog.html">Blog</a>
-                        <a class="nav-link fw-bold ms-3" href="#contact">İletişim</a>
-                        <a class="nav-link fw-bold ms-3 text-success" href="#" >Satın Al</a>
-                        </div>
-                    </div>
-                    </div>
-                </nav>
-            </section>
+           <?php
+              include('partials/_navbar.php');
+           ?>
             <!-- navbar section end -->
+
+             <!-- loader start    -->
+            <?php 
+               include('partials/_loader.php');
+            ?>
+             <!-- loader end   -->
 
             <!-- header section start -->
             <section id="header">
@@ -60,7 +26,7 @@
                             <div class="header-content mt-5">
                                 <h2 class="text-danger fw-bold display-2" >KENDİ </h2>
                                 <h1 class="text-warning fw-bold display-3">OYUNUNU YAŞA</h1>
-                                <p class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci incidunt et aperiam corporis ratione laudantium! Nostrum, cumque! Cumque blanditiis earum totam, sint odit pariatur voluptates assumenda? Vero blanditiis molestiae pariatur.</p>
+                                <p class="text-white">Kendi oyununu yaşamak için adım at, gerçek bir oyuncunun heyecanını keşfet. Etkileyici stratejiler kur, yeteneklerini geliştir ve zafere giden yolda kendi hikayeni yaz.</p>
                                 <a href="#" class="text-decoration-none text-success fs-3"><i class="fa fa-play" aria-hidden="true"></i> Oyun Videosu</a>   
                             </div>
                         </div>
@@ -87,39 +53,39 @@
                     <div class="services-card">
                         <div class="lines"></div>
                         <div class="imgBx">
-                            <img src="img/creativity.png">
+                            <img src="img/strateji.avif" class="img-fluid">
                         </div>
                         <div class="content">
                             <div class="details">
-                                <h2>Design</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi numquam vel fugit, voluptate nobis sit.</p>
-                                <a href="#">Read More</a>
+                                <h2>Strateji</h2>
+                                <p>Kendi stratejik hamlelerinizle oyunu kontrol altına alın!</p>
+                                <a href="index.php#contact" class="btn btn-outline-success">Satın AL</a>
                             </div>
                         </div >
                     </div>
                     <div class="services-card">
                         <div class="lines"></div>
                         <div class="imgBx">
-                            <img src="img/coding.png">
+                            <img src="img/talent.jpg">
                         </div>
                         <div class="content">
                             <div class="details">
-                                <h2>Launch</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi numquam vel fugit, voluptate nobis sit.</p>
-                                <a href="#">Read More</a>
+                                <h2>Yetenek</h2>
+                                <p>Yeteneklerinizi ve stratejinizi konuşturarak masanın hakimi olun!</p>
+                                <a href="index.php#contact" class="btn btn-outline-success">Satın Al</a>
                             </div>
                         </div >
                     </div>
                     <div class="services-card">
                         <div class="lines"></div>
                         <div class="imgBx">
-                            <img src="img/rocket.png">
+                            <img src="img/hedef.jpg">
                         </div>
                         <div class="content">
                             <div class="details">
-                                <h2>Design</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi numquam vel fugit, voluptate nobis sit.</p>
-                                <a href="#">Read More</a>
+                                <h2>Hedef</h2>
+                                <p>Zafere giden yolda adımlarınızı belirleyerek kazanan olun!</p>
+                                <a href="index.php#contact" class="btn btn-outline-success">Satın Al</a>
                             </div>
                         </div >
                     </div>
@@ -144,7 +110,12 @@
                         <div class="col-md-7 mb-5 ">
                             <div class="header-content mt-5">
                                 <h2 class="text-warning fw-bold display-2" >Biz Kimiz </h2>
-                                <p class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci incidunt et aperiam corporis ratione laudantium! Nostrum, cumque! Cumque blanditiis earum totam, sint odit pariatur voluptates assumenda? Vero blanditiis molestiae pariatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos nulla quo itaque voluptatem doloribus iste earum facilis laborum harum labore a totam, explicabo et dolorem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Est assumenda nobis libero dignissimos iure velit earum ut ipsa consectetur veniam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci incidunt et aperiam corporis ratione laudantium! Nostrum, cumque! Cumque blanditiis earum totam, sint odit pariatur voluptates assumenda? Vero blanditiis molestiae pariatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos nulla quo itaque voluptatem doloribus iste earum facilis laborum harum labore a totam, explicabo et dolorem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Est assumenda nobis libero dignissimos iure velit earum ut ipsa consectetur veniam.</p>
+                                <p class="text-white">GoalRush, masaüstü futbol oyunlarında öncü bir marka olarak ilerliyor. Sunduğumuz oyunlar, kalite, dayanıklılık ve eğlenceyi bir araya getirerek aileleri, arkadaşları ve iş arkadaşlarını bir araya getirmeyi daha keyifli hale getiriyor.
+                                <br><br>
+                                Misyonumuz, insanlara dijital dünyadan biraz uzaklaşma fırsatıtanıyarak, fiziksel oyun deneyimini keşfetmelerini sağlamak. GoalRusholarak, masaüstü futbol oyunlarıyla insanların etkileşimini artırarakgüçlü bağlar kurmalarına katkı sağlıyoruz.
+                                <br><br>
+                                Oyunlarımız, sadece eğlenceli bir zaman geçirmenin ötesine geçerek, aynı zamanda sosyal bağları güçlendirmek adına bir araç görevi görüyor. GoalRush, insanların bir araya gelerek keyifli anlar yaşamalarını ve unutulmaz deneyimler biriktirmelerini amaçlayan bir platformdur.
+                                </p>
                                 <a href="#" class="btn btn-outline-success text-white px-5 py-2 fs-3 fw-bold"> Satın Al</a>
                             </div>
                         </div>
@@ -182,7 +153,7 @@
                                 </div>
                                 <div class="text ms-4">
                                     <h3 class="fs-5 text-warning">Email</h3>
-                                    <p class="text-danger fs-4 fw-bold">hashus5021@gmail.com</p>
+                                    <p class="text-danger fs-4 fw-bold"> bm.gozdeyavuz@gmail.com</p>
                                 </div>
                             </div>
                             <div class="contact-item d-flex mb-3">
@@ -191,7 +162,7 @@
                                 </div>
                                 <div class="text ms-4">
                                     <h3 class="fs-5 text-warning">Telefon</h3>
-                                    <p class="text-danger fs-4 fw-bold">05373932449</p>
+                                    <p class="text-danger fs-4 fw-bold">0 541 506 26 44</p>
                                 </div>
                             </div>
                             <div class="contact-item d-flex mb-3">
@@ -200,7 +171,7 @@
                                 </div>
                                 <div class="text ms-4">
                                     <h3 class="fs-5 text-warning">Adres</h3>
-                                    <p class="text-danger fs-4 fw-bold">Afyonkarahisar</p>
+                                    <p class="text-danger fs-4 fw-bold">Antalya/Türkiye</p>
                                 </div>
                             </div>
                         </div>
@@ -225,6 +196,9 @@
                                             <textarea placeholder="Mesajınız" rows="5" name="message" class="form-control form-control-lg fs-6 border-0 shadow-sm" required ></textarea>
                                         </div>
                                     </div>
+                                    <div class="form-group mb-3">
+                                        <div class="g-recaptcha"            data-sitekey="6LcLuUApAAAAAPuzP2havi5Z1eOQVL8VZA8QiT7X"></div>
+                                    </div>
                                     <div class="row">
                                         <div class="col-lg-12 ">
                                             <button type="submit" class="btn btn-outline-success px-4 py-2">Mail Gönder</button>
@@ -239,54 +213,27 @@
             <!-- contact section end  -->
 
             <!-- footer section start  -->
-            <footer>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-12 mt-3 mb-3">
-                            <div class="social-links d-flex justify-content-center">
-                                <a href=""><i class="fa-brands fa-instagram fa-2x text-danger ms-2"></i></a>
-                                <a href=""><i class="fa-brands fa-youtube fa-2x text-danger ms-2"></i></a>
-                                <a href=""><i class="fa-brands fa-twitter fa-2x text-danger ms-2"></i></a>
-                            </div>
-                        </div>
-                        <div class="col-lg-12 footer-text">
-                        <p class="m-0 text-center text-danger fw-bold mb-4 ">&copy; 2023 GoalRush</p>
-                        </div>
-                    </div>
-                </div>
-            </footer>
+             <?php
+                include('partials/_footer.php')
+             ?>
         
             <!-- footer section end  -->
 
-
-    <script>
-        function rotateImage(event) {
-        var image = document.getElementById("interactive-image");
-
-        var containerWidth = event.currentTarget.clientWidth;
-        var containerHeight = event.currentTarget.clientHeight;
-
-        var x = (event.offsetX - containerWidth / 2) / containerWidth; // -0.5 ile 0.5 arasında değer
-        var y = (event.offsetY - containerHeight / 2) / containerHeight; // -0.5 ile 0.5 arasında değer
-
-        var rotationX = y * 30; // X ekseni için 30 derece döndürme (isteğe bağlı)
-        var rotationY = x * 30; // Y ekseni için 30 derece döndürme (isteğe bağlı)
-        var rotationZ = (x + y) * 30; // Z ekseni için 30 derece döndürme (isteğe bağlı)
-
-        image.style.transform = "rotateX(" + rotationX + "deg) rotateY(" + rotationY + "deg) rotateZ(" + rotationZ + "deg)";
+            <script>
+    if (window.location.hash) {
+        var hash = window.location.hash.substring(1);
+        var element = document.getElementById(hash);
+        if (element) {
+            element.scrollIntoView();
+        }
     }
-    </script>
-        <script src="three.js"></script>
-        <script src="script.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+</script>
 
-   
-        
-        
-    
-    
-        
-        
+        <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+        <script src="js/rotateimg.js"></script>
+        <script src="js/three.js"></script>
+        <script src="js/scene.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
     
     </body>
     </html>
--- send_email.php
+++ send_email.php
@@ -1,76 +1,78 @@
-    <?php 
+<?php
 
-    session_start();
+session_start();
 
-    use  PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-    require "vendor/autoload.php";
+require "vendor/autoload.php";
 
-    if(isset($_POST)){
-        if($_POST["fullname"] && $_POST["email"] && $_POST["subject"] && $_POST["message"]){
-        //    Mail gönderme işlemleri 
+$recaptchaResponse = isset($_POST['g-recaptcha-response']) ? $_POST['g-recaptcha-response'] : null;
 
-        $mail = new PHPMailer();
+if (isset($_POST)) {
+    if (empty($recaptchaResponse)) {
+        // reCAPTCHA işaretlenmediğinde uyarı ver
+        $alert = array(
+            "message" => "reCAPTCHA işaretlenmedi",
+            "type" => "danger"
+        );
+    } else {
+        if ($_POST["fullname"] && $_POST["email"] && $_POST["subject"] && $_POST["message"]) {
+            // Mail gönderme işlemleri 
+            $mail = new PHPMailer();
 
-        try{
-            // server ayarları 
-            $mail->SMTPDebug = 2;
-            $mail ->isSMTP();
-            $mail->Host = "ssl://smtp.gmail.com";
-            $mail->SMTPAuth = true;
-            $mail->Username = "hashus5021@gmail.com";
-            $mail->Password = "ibbs rvgj gceh ffav";
-            $mail->CharSet = "utf-8";
-            $mail->SMTPSecure = "ssl";
-            $mail->Port = 465;
+            try {
+                // server ayarları 
+                $mail->SMTPDebug = 2;
+                $mail->isSMTP();
+                $mail->Host = "ssl://smtp.gmail.com";
+                $mail->SMTPAuth = true;
+                $mail->Username = "hashus5021@gmail.com";
+                $mail->Password = "ibbs rvgj gceh ffav";
+                $mail->CharSet = "utf-8";
+                $mail->SMTPSecure = "ssl";
+                $mail->Port = 465;
 
-            // Alıcı ayarları 
-            $mail->addAddress("hashus5021@gmail.com",$_POST["fullname"]);
-            $mail->setFrom($_POST["email"],"");
+                // Alıcı ayarları 
+                $mail->addAddress("hashus5021@gmail.com", $_POST["fullname"]);
+                $mail->setFrom($_POST["email"], "");
 
-            // Gönderi ayarları 
-            $mail->isHTML();
-            $mail->Subject = $_POST["subject"];
-            $body = "Gönderen Adı: " . $_POST["fullname"] . "<br>";
-            $body .= "Gönderen E-posta: " . $_POST["email"] . "<br>";
-            $body .= "Mesaj: " . $_POST["message"];
+                // Gönderi ayarları 
+                $mail->isHTML();
+                $mail->Subject = $_POST["subject"];
+                $body = "Gönderen Adı: " . $_POST["fullname"] . "<br>";
+                $body .= "Gönderen E-posta: " . $_POST["email"] . "<br>";
+                $body .= "Mesaj: " . $_POST["message"];
 
-            $mail->Body = $body;
+                $mail->Body = $body;
 
-
-            if($mail->send()){
+                if ($mail->send()) {
+                    $alert = array(
+                        "message" => "Mail Başarılı Bir Şekilde Gönderildi",
+                        "type" => "success"
+                    );
+                } else {
+                    $alert = array(
+                        "message" => "Mail Gönderilmedi",
+                        "type" => "danger"
+                    );
+                }
+            } catch (Exception $e) {
                 $alert = array(
-                    "message" => "Mail Başarılı Bir Şekilde Gönderildi",
-                    "type" => "success"
-                );
-            } else {
-                $alert = array(
-                    "message" => "Mail Gönderilmedi",
+                    "message" => $e->getMessage(),
                     "type" => "danger"
                 );
             }
-
-        } catch(Exception $e){
-            $alert = array(
-                "message" => $e->getMessage(),
-                "type" => "danger"
-            );
-        }
-
-    
         } else {
-            // bu alanları doldurun hatası
-
+            // boş alan hatası
             $alert = array(
                 "message" => "Lütfen Tüm ALANLARI Doldurun",
                 "type" => "danger"
             );
-
-          
-            
         }
-        $_SESSION["alert"] = $alert;
-
-        header("Location: index.php#contact");
     }
+
+    $_SESSION["alert"] = $alert;
+    header("Location: index.php#contact");
+}
+?>
